2944f1b1a5514071cf4d3c49056abfa05a654794
Marya <maryaedurda15059927@gmail.com>
2024-09-18 11:12:52 -0300

    Pronto

--- ProjetoShow/MainPage.xaml.cs
+++ ProjetoShow/MainPage.xaml.cs
@@ -1,31 +1,13 @@
 ﻿
-using Microsoft.Maui.Controls;
-using System;
+namespace ProjetoShow;
 
-namespace ProjetoShow
-{
     public partial class MainPage : ContentPage
-    {
+   {
+    
         public MainPage()
         {
-            InitializeComponent();
+        InitializeComponent();
         }
 
-        private void OnCorrectClicked(object sender, EventArgs e)
-        {
-            // Handle the logic for "Certas" button click
-        }
-
-        private void OnRemoveWrongClicked(object sender, EventArgs e)
-        {
-            // Handle the logic for "Retirar 1 errada" button click
-        }
-
-        private void OnAskStudentClicked(object sender, EventArgs e)
-        {
-            // Handle the logic for "Universitário" button click
-        }
-    }
-}
-
+   }
 
